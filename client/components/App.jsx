3c9dab95b10ea72291276889cf5b1c7f242a6eea
Michael Lav <mikelav258@yahoo.com>
2023-06-05 16:29:29 -0700

    added sql schema

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import Login from './Login'
 import MainPage from './MainPage'
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-    
+
   return(
     <>
       {!isLoggedIn && <Login />}
---
+++ server/models/userModel.js
@@ -0,0 +1,3 @@
+const { Pool } = require('pg');
+
+const PG_URI = process.env.PG_URI;
